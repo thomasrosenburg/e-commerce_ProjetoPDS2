f143bc334317ab1b8db167f3adf3c03851350a1c
Thomas Costa Rosenburg Alvares <thomasrosenburg@ufmg.br>
2022-12-04 09:06:48 -0300

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -3,16 +3,18 @@
 
 #include "endereco.h"
 #include "usuario.h"
-
+//subclasse da classe Usuario
 class Cliente:public Usuario
-{
+{   /*declaração das informações privadas do cliente
+    note que o nome e o login foram herdados da classe Usuario.
+*/
     private:
     std::string _email;
     Endereco _endereco; 
     unsigned int _celular;
-
+    //declara os métodos que o cliente pode executar.
     public:
-
+    //constrói um cliente
     Cliente( std:: string nome, 
      std:: string login, 
      std::string email, 
@@ -22,19 +24,21 @@ class Cliente:public Usuario
     Estado estado,
     unsigned int numero,
     unsigned int complemento);
-
+    //acessa a variável celular do cliente.
     unsigned int get_celular();
-
+    //acessa a variável e-mail do cliente.
     std:: string get_email();
-
+    //acessa a variável endereco do cliente.
     Endereco get_endereco();
-
+    //imprime na tela as informações atreladas ao cliente.
     void imprime_cliente();
-    
+    //registra um e-mail para o cliente no site.
     void set_email(std::string);
+    //registra um celular para o cliente no site.
     void set_celular(unsigned int cel);
+    //registra um endereco para o cliente no site.
     void set_endereco(Endereco end);
-    
+    //determina o tipo de acesso que o cliente tem no site.
     virtual Acesso acesso_site() const override;
 
 };
