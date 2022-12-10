6ccc79e2ca21452572578bd41d7029b245567597
joseane13 <117231660+joseane13@users.noreply.github.com>
2022-12-10 12:40:05 -0300

    Update test_usuario.cpp

--- test_usuario.cpp
+++ test_usuario.cpp
@@ -1,13 +1,13 @@
 #include "usuario.h"
 #include "doctest.h"
-
+//Testa o construtor do usuario e enum Acesso
 TEST_CASE("Testa Usuario Construtor") {
   Usuario usuario = Usuario("Taylor", "ts1989");
   CHECK_EQ("Taylor", usuario.get_nome());
   CHECK_EQ("ts1989", usuario.get_login());
   CHECK_EQ(2, usuario.acesso_site());
 }
-
+//Testa o caso de redefinição de nome e login
 TEST_CASE("Testa Set Usuario"){
     Usuario usuario = Usuario("Harry", "hs1964");
     usuario.set_nome("Styles");
