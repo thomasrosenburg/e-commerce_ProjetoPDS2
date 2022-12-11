6a43b2159e365e61107e354337729b66574e92a9
joseane13 <117231660+joseane13@users.noreply.github.com>
2022-12-10 23:23:38 -0300

    Update test_produto.cpp

---
+++ test_produto.cpp
@@ -0,0 +1,18 @@
+#include "produto.h"
+#include "doctest.h"
+
+TEST_CASE("Testa produto construtor"){
+    Produto produto = Produto("Blusa nike", blusa, 1, 89.90, 1, "M", "Preto");
+    CHECK_EQ(0, produto.get_tipo());
+    CHECK_EQ("Blusa nike", produto.get_modelo());
+    CHECK_EQ("M", produto.get_tamanho());
+    CHECK_EQ(1, produto.get_codigo());
+    CHECK_EQ("Preto", produto.get_cor());
+    CHECK_EQ(1, produto.get_quantidade());
+}
+
+TEST_CASE("Testa produto construtor"){
+    Produto produto = Produto("Blusa nike", blusa, 1, 89.90, 3, "M", "Preto");
+    produto.alterar_quantidade(1);
+    CHECK_EQ(2, produto.get_quantidade());
+}
