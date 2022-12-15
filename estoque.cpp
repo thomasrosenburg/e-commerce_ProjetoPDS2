01fe6467193757e42bcfa024ed1280382ba87e00
alicelacerda <117699065+alicelacerda@users.noreply.github.com>
2022-12-15 13:55:56 -0300

    Update estoque.cpp

--- estoque.cpp
+++ estoque.cpp
@@ -111,3 +111,27 @@ if((*it).get_tipo()==4)
         }
 }
 } 
+void Estoque::inicializa_estoque()
+{
+
+    adiciona_produto(Produto("Cropped",blusa,20267, 50.00, 25, "P", "preto"));
+    adiciona_produto(Produto("Cropped",blusa, 20268,50.00, 30, "M", "preto"));
+    adiciona_produto(Produto("Cropped",blusa, 20269, 50.00, 15, "G", "preto"));
+    adiciona_produto(Produto("Blusa Manga Comprida",blusa, 20777, 75.00, 25, "M", "azul"));
+    adiciona_produto(Produto("Regata",blusa,2980, 30.00, 25,"G", "branca"));
+    adiciona_produto(Produto("Regata",blusa,2981, 30.00, 67, "M", "branca"));
+    adiciona_produto(Produto("Camiseta",blusa, 2997, 35.00, 25,"P", "azul"));
+    adiciona_produto(Produto("Camiseta",blusa, 2999, 35.00, 100, "G", "azul"));
+    adiciona_produto(Produto("Bota", sapato,1234, 100.00, 17, "36", "preta"));
+    adiciona_produto(Produto("Bota", sapato, 1235, 100.00, 25, "37", "preta"));
+    adiciona_produto(Produto("Calça Jeans", calca, 4532, 125.00, 1, "M", "azul"));
+    adiciona_produto(Produto("Calça Jeans", calca, 4533, 125.00, 2, "M"," preta"));
+    adiciona_produto(Produto("Tênis", sapato, 1256, 150.00, 4, "40", "branco"));
+    adiciona_produto(Produto("Tênis", sapato, 1257, 150.00, 8, "42", "preto"));
+    adiciona_produto(Produto("Shorts saia", shorts, 3890, 50.00, 23, "M", "verde"));
+    adiciona_produto(Produto("Shorts academia", shorts, 3891, 45.00, 2, "G", "lilás"));
+    adiciona_produto(Produto("Shorts Jeans", shorts, 3892, 65.00,8, "M", "azul"));
+    adiciona_produto(Produto("Vestido Longo", vestido, 7896, 120.00, 2, "P", "vermelho"));
+    adiciona_produto(Produto("Vestido tubinho", vestido, 7988, 80.00, 8, "M", "preto"));
+    adiciona_produto(Produto("Vestido tubinho", vestido, 7999, 85.00, 9, "G", "branco"));
+}
