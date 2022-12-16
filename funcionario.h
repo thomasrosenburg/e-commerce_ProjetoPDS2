106c6af1f7b1d6d56d4da87622b2e4844dc40ea1
guibacha <119077025+guibacha@users.noreply.github.com>
2022-12-16 10:20:46 -0300

    Add files via upload

---
+++ funcionario.h
@@ -0,0 +1,15 @@
+#ifndef FUNCIONARIO_H
+#define FUNCIONARIO_H
+
+#include "usuario.h"
+
+class Funcionario: public Usuario{
+    private:
+    unsigned int _registro;
+    public:
+    Funcionario(std:: string _nome, std:: string login, unsigned int _registro);
+    unsigned int get_registro();
+    virtual Acesso acesso_site() const override;
+};
+
+#endif 
