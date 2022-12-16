62bf7a7e9369b9210db47b2cff394a3397fb2654
guibacha <119077025+guibacha@users.noreply.github.com>
2022-12-16 10:20:18 -0300

    Add files via upload

---
+++ funcionario.cpp
@@ -0,0 +1,19 @@
+#include "funcionario.h"
+
+Funcionario::Funcionario(
+    std::string nome,
+    std:: string login,
+    unsigned int registro
+    ): Usuario(nome,login),
+    _registro(registro)
+    {
+
+    };
+unsigned int Funcionario::get_registro()
+{
+    return _registro;
+}
+
+Acesso Funcionario::acesso_site() const {
+    return Acesso::AMPLO;
+}
