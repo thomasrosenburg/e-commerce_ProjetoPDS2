bd374bc4df4f0b1cf3b21997da50400b46bf8420
alicelacerda <117699065+alicelacerda@users.noreply.github.com>
2022-12-16 18:43:00 -0300

    Update pedido.h

--- pedido.h
+++ pedido.h
@@ -4,7 +4,7 @@
 #include "produto.h"
 #include "cliente.h"
 
-#include <map>
+#include <list>
 //métodos de pagamento.
 enum Pagamento{
     cartao_credito, cartao_debito, boleto, pix, transferencia_bancaria
@@ -14,22 +14,28 @@ class Pedido
 {
     //declara as informações privadas do TAD pedido
     private:
-    //map que organiza os pedidos feitos, como um carrinho.
-    std:: map<const Produto, unsigned int> _produtos_pedidos;
+    //list que organiza os pedidos feitos, como um carrinho.
+    std:: list<Produto> _produtos_pedidos;
 
     public:
     Cliente _cliente;
-    //adiciona um produto ao carrinho de compras.
+    
     Pedido();
+    //adiciona um produto ao carrinho de compras.
     void adicionar_produto(Produto p, unsigned int qtd);
+    
     //adiciona o cliente que está comprando.
     void adicionar_cliente( Cliente cliente);
+    
     //imprime as informações do cliente e do carrinho de compras.
     void imprimir_carrinho();
+    
     //retorna o valor do frete a ser pago.
     std:: string calcular_frete();
+    
     //calcula o total da compra.
     double  calcular_total();
+    
     //retorna a forma de pagamento selecionada.
     std:: string selecionar_pagamento(Pagamento forma_selecionada);
 
