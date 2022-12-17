b4bd95d100bbf6e9d5179f97d3e6fc41d4d2c71f
alicelacerda <117699065+alicelacerda@users.noreply.github.com>
2022-12-16 21:45:30 -0300

    Add files via upload

---
+++ banco_cliente.cpp
@@ -0,0 +1,26 @@
+#include "banco_cliente.h"
+
+void Banco_Cliente::adiciona_cliente(Cliente cliente)
+{
+    _banco_cliente.push_front(cliente);
+}
+
+
+Cliente Banco_Cliente::retorna_cliente(std:: string login)
+{
+    for(auto it=_banco_cliente.begin(); it!=_banco_cliente.end(); it++)
+    {
+        if((*it).get_login()==login)
+        {
+            return (*it);
+        }
+    }
+}
+
+void Banco_Cliente:: Inicializa_Clientes()
+{
+    adiciona_cliente(Cliente("Raquel", "raquellacerda", "raquellacerda@gmail.com", "9961-3850", "30140111", "Belo Horizonte", Estado::MG, 799, 1701));
+    adiciona_cliente(Cliente("Marcos", "marcoscunha", "marcoscunha@gmail.com", "8776-7899", "56771223", "Recife", Estado::PE, 146, 801));
+    adiciona_cliente(Cliente("Breno", "brenogomes", "brenogomes@gmail.com", "8992-1415", "12286789", "Porto Alegre", Estado::RS, 807, 1102));
+    adiciona_cliente(Cliente("Leonardo", "leonardopitella", "leonardopitella@gmail.com", "9923-5789", "78990076", "Goiânia", Estado::GO, 1233, 102));
+}
