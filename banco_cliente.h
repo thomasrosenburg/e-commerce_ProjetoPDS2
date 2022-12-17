b4990cd69985c1701a2014ad460950588fe49c48
alicelacerda <117699065+alicelacerda@users.noreply.github.com>
2022-12-16 21:44:43 -0300

    Add files via upload

---
+++ banco_cliente.h
@@ -0,0 +1,26 @@
+#ifndef  BANCO_CLIENTE_H
+#define BANCO_CLIENTE_H
+#include "cliente.h"
+
+#include <string>
+#include <list>
+
+class Banco_Cliente
+{
+    private:
+
+    std::list<Cliente> _banco_cliente;
+
+    public:
+    //função que simula um banco de clientes registrados no site
+    void Inicializa_Clientes();
+    //adiciona cliente ao registro do site
+    void adiciona_cliente(Cliente cliente);
+    //caso o cliente esteja registrado no site, ele é retornado
+    Cliente retorna_cliente(std::string login);
+
+
+};
+
+
+#endif
