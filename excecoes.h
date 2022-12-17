c05824f012e7869fc481230bbc965e43f5ce067b
Thomas Costa Rosenburg Alvares <thomasrosenburg@ufmg.br>
2022-12-17 12:38:46 -0300

    Add files via upload

---
+++ excecoes.h
@@ -0,0 +1,24 @@
+#ifndef EXCECOES_H
+#define EXCECOES_H
+
+#include<iostream>
+using namespace std;
+
+class excecoes_login_cliente :public exception
+
+{
+    public:
+    virtual const char* what() const noexcept override;
+    
+};
+class excecoes_operacoes :public exception
+{
+    public:
+    virtual const char* what() const noexcept override;
+};
+class excecoes_codigo : public exception
+{
+    public:
+    virtual const char* what() const noexcept override;
+};
+#endif
