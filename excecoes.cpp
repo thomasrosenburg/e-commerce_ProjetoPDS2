e4e64e402fc8d5221c36f2df950ba35f01d894fd
Thomas Costa Rosenburg Alvares <thomasrosenburg@ufmg.br>
2022-12-17 20:39:13 -0300

    Update excecoes.cpp

--- excecoes.cpp
+++ excecoes.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 const char* excecoes_login_cliente:: what() const noexcept
 {
-    return "Login invalido!!"
+    return "Login invalido!;"
 }
 const char* excecoes_operacoes:: what() const noexcept
 {
-    return "operacao invalida"
+    return "operacao invalida;"
 }
 const char* excecoes_codigo:: what() const noexcept
 {
-    return "produto não encontrado"
+    return "produto não encontrado;"
 }
