72fc96caf6926944d9fe5562b6ac272079365d56
Thomas Costa Rosenburg Alvares <thomasrosenburg@ufmg.br>
2022-12-17 12:39:15 -0300

    Add files via upload

---
+++ excecoes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "excecoes.h"
+
+using namespace std;
+
+const char* excecoes_login_cliente:: what() const noexcept
+{
+    return "Login invalido!!"
+}
+const char* excecoes_operacoes:: what() const noexcept
+{
+    return "operacao invalida"
+}
+const char* excecoes_codigo:: what() const noexcept
+{
+    return "produto não encontrado"
+}
