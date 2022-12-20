6e40e7a455262e4f70646dcc43e035c546369b55
alicelacerda <117699065+alicelacerda@users.noreply.github.com>
2022-12-20 20:59:04 -0300

    Update banco_cliente.h

--- banco_cliente.h
+++ banco_cliente.h
@@ -6,28 +6,41 @@
 
 #include <string>
 #include <list>
-//@class Banco_Cliente
+/**
+ * @class Banco_Cliente
+ *
+ * @brief Esta classe é responsável simular um banco de dados de cliente
+ * e introduzir funcoes relacionadas a esse banco.
+ */
 class Banco_Cliente: public Banco_Dados
 {
     private:
+   /**
+   * @brief Lista de Clientes que representa o banco de dados.
+   */
 
     std::list<Cliente> _banco_cliente;
 
     public:
-    /* @brief função que simula um banco de clientes registrados no site
+    /**
+    * @brief função que simula um banco de clientes registrados no site. 
+    * Essa funcao concretiza o metodo virtual da classe Banco_Dados.
     */
     virtual void inicializa() override;
-    /* @brief adiciona cliente ao registro do site
-     * @param o Cliente cliente a ser adicionado
-     */
+    /**
+    * @brief adiciona cliente ao registro do site
+    * @param cliente Objeto Cliente que será adicionado ao banco de dados.
+    */
     void adiciona_cliente(Cliente cliente);
-    /* @brief caso o cliente esteja registrado no site, ele é retornado
-     * @param string login do cliente
+    /**
+     * @brief caso o cliente esteja registrado no site, ele é retornado
+     * @param std::string login do cliente
+     * @return Cliente Retorna o cliente que corresponde a aquele login.
      */
     Cliente retorna_cliente(std::string login);
-    /* @brief confere se o cliente esta ou nao ja registrado
-     * @param string login do cliente testado
-     * @return true retorna verdadeiro caso o login seja encontrado no banco de dados
+    /**
+     * @brief confere se o cliente esta ou nao ja registrado
+     * @param string login do cliente testado.
      * @return false retorna verdadeiro caso o login nao seja encontrado no banco de dados
      */
     virtual bool confere_login(std::string login) override;
