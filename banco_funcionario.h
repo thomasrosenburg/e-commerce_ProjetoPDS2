03cf2a28cef917cb34de5b6e397d980b89c24760
Thomas Costa Rosenburg Alvares <thomasrosenburg@ufmg.br>
2022-12-20 19:30:50 -0300

    Update banco_funcionario.h

--- banco_funcionario.h
+++ banco_funcionario.h
@@ -12,12 +12,27 @@
 class Banco_Funcionario: public Banco_Dados
 {
     private:
-
+    /**
+     *@brief lista de funcionários da loja
+     */
     std::list<Funcionario> _banco_funcionarios;
     
     public:
+    /**
+     *@brief adiciona um novo cadastro de funcionário
+     *@param nome Nome do novo funcionário
+     *@param login Login do novo funcionario
+     *@param registro Registro do novo funcionario
+     */
     void adiciona_funcionario(std:: string nome, std:: string login, unsigned int registro);
-    virtual bool confere_login(std::string login) override; 
+    /**
+     *@brief confere se o login está na lista
+     *@param login Login fornecido
+     */
+    virtual bool confere_login(std::string login) override;
+    /**
+     *@brief inicializa o banco de dados de funcionarios 
+     */
     virtual void inicializa() override;
 
 
