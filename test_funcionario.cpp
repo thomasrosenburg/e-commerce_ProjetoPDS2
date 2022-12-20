42eea106acef5c1fe9926e90d619232e5273b913
joseane13 <117231660+joseane13@users.noreply.github.com>
2022-12-19 21:29:19 -0300

    Add files via upload

---
+++ test_funcionario.cpp
@@ -0,0 +1,9 @@
+#include "../include/funcionario.h"
+#include "../third_party/doctest.h"
+
+TEST_CASE("Testa construtor funcionario"){
+    Funcionario funcionario = Funcionario("Thomas", "THOMPZERA", 13);
+    CHECK_EQ(13, funcionario.get_registro());
+    CHECK_EQ("Thomas", funcionario.get_nome());
+    CHECK_EQ("THOMPZERA", funcionario.get_login());
+}
