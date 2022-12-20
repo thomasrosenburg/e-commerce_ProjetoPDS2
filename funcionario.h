c188b3cd3f90eaaa569abd747c94f85ad929fd2f
Thomas Costa Rosenburg Alvares <thomasrosenburg@ufmg.br>
2022-12-20 19:53:12 -0300

    Update funcionario.h

--- funcionario.h
+++ funcionario.h
@@ -2,13 +2,28 @@
 #define FUNCIONARIO_H
 
 #include "usuario.h"
-
+//@class Funcionario
 class Funcionario: public Usuario{
     private:
+    ///< o registro do funcionario
     unsigned int _registro;
     public:
+    /**
+    * @brief constroi um funcionario
+    * @param _nome Nome do funcionario
+    * @param login Login do funcionario
+    * @param _registro Registro do funcionario
+    */
     Funcionario(std:: string _nome, std:: string login, unsigned int _registro);
+    /**
+    * @brief acessa a variavel registro
+    * @return unsigned int retorna o registro
+    */
     unsigned int get_registro();
+    /**
+    * @brief Caracteriza o acesso ao site
+    * @return string retorna o tipo de acesso que o funcionario tem
+    */
     virtual Acesso acesso_site() override;
 };
 
