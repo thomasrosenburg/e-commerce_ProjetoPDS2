f4b745b733e5b6b89ec02797ba65abe3eaa56e25
alicelacerda <117699065+alicelacerda@users.noreply.github.com>
2022-12-20 21:23:26 -0300

    Update estoque.h

--- estoque.h
+++ estoque.h
@@ -3,42 +3,67 @@
 
 #include <list>
 #include "produto.h"
-//@class Estoque
+/**
+ * @class Estoque
+ *
+ * @brief Esta classe é responsável por criar o estoque
+ * e introduzir as funções para administrar essa classe.
+ */
 class Estoque
 {
     private:
-    //@brief lista de produtos que compõem o estoque da loja.
+   /**
+   * @brief lista de produtos que compõem o estoque da loja.
+   */
     std::list<Produto> lista_produtos;
    
     public:
-     //@brief ordena e imprime na tela os produtos do estoque na ordem crescente de preço.
+   /**
+   * @brief ordena e imprime na tela os produtos do estoque na ordem crescente de preço.
+   */
     void ordena_preco(); 
-    /** @brief adiciona um novo produto ao estoque.
+    /** 
+     *@brief adiciona um novo produto ao estoque.
      * @param prod_novo Produto novo a ser adicionado.
      */
     void adiciona_produto(Produto prod_novo);
-    /** @brief retira um produto do estoque, por exemplo se o próprio esgotar.
+    /** 
+     *@brief retira um produto do estoque, por exemplo se o próprio esgotar.
      * @param prod Produto a ser retirado
      */
     void retira_produto(Produto prod);
-    //@brief exibe apenas produtos do tipo blusa para o cliente.
+     /** 
+     *@brief exibe apenas produtos do tipo blusa para o cliente.
+     */
     void exibir_blusa();
-    // @brief exibe apenas produtos do tipo short para o cliente.
+     /** 
+     *@brief exibe apenas produtos do tipo short para o cliente.
+     */
     void exibir_shorts();
-    // @brief exibe apenas produtos do tipo sapato para o cliente.
+     /** 
+     *@brief exibe apenas produtos do tipo sapato para o cliente.
+     */ 
     void exibir_sapatos();
-    // @brief exibe apenas produtos do tipo vestido para o cliente.
+    /** 
+     *@brief exibe apenas produtos do tipo vestido para o cliente.
+     */
     void exibir_vestidos();
-    // @brief exibe apenas produtos do tipo calça para o cliente.
+     /** 
+     *@brief exibe apenas produtos do tipo calca  para o cliente.
+     */
     void exibir_calca();
-    //@brief imprime na tela todos os produtos presentes no estoque
+    /** 
+     *@brief exibe todos os produtos do estoque.
+     */
     void imprimir_todos();
     /**
     *@brief função criada para encontrar um produto no estoque, pelo seu codigo.
     *@param codigo Codigo do produto.
     */
     Produto find_codigo(unsigned int codigo);
-    //@brief função criada para simular um banco de dados
+     /** 
+     *@brief função criada para simular um banco de dados.
+     */
     void inicializa_estoque();
     /**
     *@brief  confere se o código dado é válido
