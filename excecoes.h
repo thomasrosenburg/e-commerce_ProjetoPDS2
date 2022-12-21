25c1157fe55ec5ffd20ee4dba5eb00102c74254e
alicelacerda <117699065+alicelacerda@users.noreply.github.com>
2022-12-20 21:56:59 -0300

    Update excecoes.h

--- excecoes.h
+++ excecoes.h
@@ -3,6 +3,12 @@
 
 #include<iostream>
 using namespace std;
+/**
+ * @class excecoes_login_cliente
+ *
+ * @brief Esta classe é responsável lançar exceção quando o login 
+ * digitado nao estiver registrado no banco de dados.
+ */
 
 class excecoes_login_cliente :public exception
 
@@ -11,31 +17,67 @@ class excecoes_login_cliente :public exception
     virtual const char* what() const noexcept override;
     
 };
+/**
+ * @class excecoes_operacoes
+ *
+ * @brief Esta classe é responsável lançar exceção quando a
+ * operacao realizada e incompativel.
+ */
 class excecoes_operacoes :public exception
 {
     public:
     virtual const char* what() const noexcept override;
 };
+/**
+ * @class excecoes_codigo
+ *
+ * @brief Esta classe é responsável lançar exceção quando o codigo
+ * digitado nao estiver registrado no banco de dados.
+ */
 class excecoes_codigo : public exception
 {
     public:
     virtual const char* what() const noexcept override;
 };
+/**
+ * @class excecoes_categoria
+ *
+ * @brief Esta classe é responsável lançar exceção quando a categoria 
+ * digitada for incompativel.
+ */
 class excecoes_categoria : public exception
 {
     public:
     virtual const char* what() const noexcept override;
 };
+/**
+ * @class excecoes_quuantidade
+ *
+ * @brief Esta classe é responsável lançar exceção quando a 
+ * quantidade digitada for incompativel.
+ */
 class excecoes_quantidade : public exception
 {
     public:
     virtual const char* what() const noexcept override;
 };
+/**
+ * @class excecoes_conversao_string
+ *
+ * @brief Esta classe é responsável lançar exceção quando a string
+ * digitada for incompativel aos estados.
+ */
 class excecoes_conversao_string : public exception
 {
     public:
     virtual const char* what() const noexcept override;
 };
+/**
+ * @class excecoes_conversao_estado
+ *
+ * @brief Esta classe é responsável lançar exceção quando o estado
+ * digitado for incompativel.
+ */
 class excecoes_conversao_estado : public exception
 {
     public:
