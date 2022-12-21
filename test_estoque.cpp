a5d1f614ea42fc4587ee0ba1bf3720eff5afa337
joseane13 <117231660+joseane13@users.noreply.github.com>
2022-12-21 12:00:46 -0300

    Update test_estoque.cpp

--- test_estoque.cpp
+++ test_estoque.cpp
@@ -1,33 +1,35 @@
 #include "../include/estoque.h"
 #include "../third_party/doctest.h"
 
-TEST_CASE("Testa estoque adiciona produto"){
-   Estoque estoque;
-   Produto produto = Produto("Blusa nike", blusa, 1, 89.90, 1, "M", "Preto");
-   estoque.adiciona_produto(produto);
-   Produto auxiliar = estoque.find_codigo(1);
-   CHECK_EQ(auxiliar.get_tipo(), produto.get_tipo());
-   CHECK_EQ(auxiliar.get_modelo(), produto.get_modelo());
-   CHECK_EQ(auxiliar.get_tamanho(), produto.get_tamanho());
-   CHECK_EQ(auxiliar.get_codigo(), produto.get_codigo());
-   CHECK_EQ(auxiliar.get_cor(), produto.get_cor());
-   CHECK_EQ(auxiliar._quantidade, produto._quantidade);
-   CHECK_THROWS_WITH(estoque.find_codigo(13), "código inválido!");
+TEST_CASE("Testa estoque adiciona produto")
+{
+    Estoque estoque;
+    Produto produto = Produto("Blusa nike", blusa, 1, 89.90, 1, "M", "Preto");
+    estoque.adiciona_produto(produto);
+    Produto auxiliar = estoque.find_codigo(1);
+    CHECK_EQ(auxiliar.get_tipo(), produto.get_tipo());
+    CHECK_EQ(auxiliar.get_modelo(), produto.get_modelo());
+    CHECK_EQ(auxiliar.get_tamanho(), produto.get_tamanho());
+    CHECK_EQ(auxiliar.get_codigo(), produto.get_codigo());
+    CHECK_EQ(auxiliar.get_cor(), produto.get_cor());
+    CHECK_EQ(auxiliar._quantidade, produto._quantidade);
+    CHECK_THROWS_WITH(estoque.find_codigo(13), "produto não encontrado!");
 }
 
-TEST_CASE("Testa estoque retira produto"){
-   Estoque estoque;
-   Produto produto = Produto("Blusa nike", blusa, 1, 89.90, 1, "M", "Preto");
-   estoque.adiciona_produto(produto);
-   Produto auxiliar = estoque.find_codigo(1);
-   CHECK_EQ(auxiliar.get_tipo(), produto.get_tipo());
-   CHECK_EQ(auxiliar.get_modelo(), produto.get_modelo());
-   CHECK_EQ(auxiliar.get_tamanho(), produto.get_tamanho());
-   CHECK_EQ(auxiliar.get_codigo(), produto.get_codigo());
-   CHECK_EQ(auxiliar.get_cor(), produto.get_cor());
-   CHECK_EQ(auxiliar._quantidade, produto._quantidade);
-   estoque.retira_produto(produto);
-   bool checar = estoque.confere_codigo(1);
-   CHECK_EQ(checar, false);
+TEST_CASE("Testa estoque retira produto")
+{
+    Estoque estoque;
+    Produto produto = Produto("Blusa nike", blusa, 1, 89.90, 1, "M", "Preto");
+    estoque.adiciona_produto(produto);
+    Produto auxiliar = estoque.find_codigo(1);
+    CHECK_EQ(auxiliar.get_tipo(), produto.get_tipo());
+    CHECK_EQ(auxiliar.get_modelo(), produto.get_modelo());
+    CHECK_EQ(auxiliar.get_tamanho(), produto.get_tamanho());
+    CHECK_EQ(auxiliar.get_codigo(), produto.get_codigo());
+    CHECK_EQ(auxiliar.get_cor(), produto.get_cor());
+    CHECK_EQ(auxiliar._quantidade, produto._quantidade);
+    estoque.retira_produto(produto);
+    bool checar = estoque.confere_codigo(1);
+    CHECK_EQ(checar, false);
 }
 
