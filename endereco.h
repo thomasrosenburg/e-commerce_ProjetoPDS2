0985fbfb79d60a607ff9e88acd9585c2a9b064ef
alicelacerda <117699065+alicelacerda@users.noreply.github.com>
2022-12-20 21:16:45 -0300

    Update endereco.h

--- endereco.h
+++ endereco.h
@@ -2,29 +2,58 @@
 #define ENDERECO_H
 
 #include <string>
+/** @enum Estado
+ * @brief Estado enum, corresponde aos estados brasileiros.
+ */
 enum Estado
 {
     /* SUDESTE */
-    MG, SP, RJ, ES,
+    MG, ///< Estado::MG
+    SP, ///< Estado::SP
+    RJ, ///< Estado::RJ
+    ES, ///< Estado::ES
     /* SUL*/
-    RS, PR, SC,
+    RS, ///< Estado::RS
+    PR, ///< Estado::PR
+    SC,///< Estado::SC
     /* CENTRO OESTE*/
-    MT, MS, GO,
+    MT, ///< Estado::MT
+    MS, ///< Estado::MS
+    GO,///< Estado::GO
     /* NORDESTE*/
-    BA, PI, MA, CE, RN, PB, PE, AL, SE,
+    BA, ///< Estado::BA
+    PI, ///< Estado::PI
+    MA, ///< Estado::MA
+    CE, ///< Estado::CE
+    RN, ///< Estado::RN
+    PB, ///< Estado::PB
+    PE, ///< Estado::PE
+    AL, ///< Estado::AL
+    SE,///< Estado::SE
     /* NORTE*/
-    AC, RO, AM, RR, PA, AP, TO
+    AC, ///< Estado::AC
+    RO, ///< Estado::RO
+    AM, ///< Estado::AM
+    RR, ///< Estado::RR
+    PA, ///< Estado::PA
+    AP, ///< Estado::AP
+    TO ///< Estado::TO
 
-}; // @class Endereco
+}; 
+/**
+ * @class Endereco
+ *
+ * @brief Esta classe é responsável por criar o objeto endereco
+ * e introduzir as funções relativas a essa classe.
+ */
 class Endereco{
-    //declara as informações privadas do endereço.
+    
     private:
-    std::string _CEP;
-    std::string _cidade;
-    Estado _estado;
-    unsigned int _numero;
-    unsigned int _complemento;
-    //declara os métodos relacionados ao endereço.
+    std::string _CEP; ///< CEP.
+    std::string _cidade; ///< Cidade.
+    Estado _estado; ///< Estado.
+    unsigned int _numero; ///< numero da residencia.
+    unsigned int _complemento; ///< complemento.
     public:
     /**
      *@brief constroi uma classe endereco
@@ -39,15 +68,30 @@ class Endereco{
     Estado estado,
     unsigned int numero,
     unsigned int complemento);
-    //@brief acessa a variável CEP do endereço.
+    /**
+    *@brief acessa a variável CEP do endereço.
+    *@return std::string Retorna o CEP.
+    */
     std::string get_CEP(); 
-    //@brief acessa a variável cidade do endereço.
+    /**
+    *@brief acessa a variável cidade do endereço.
+    *@return std::string Retorna a Cidade.
+    */
     std::string get_cidade();
-    //@brief determina o estado que o endereço se encontra.
+    /**
+    *@brief determina o estado que o endereço se encontra.
+    *@return Estado Retorna o Estado.
+    */
     Estado get_estado();
-    //@brief acessa a variável numero do endereço.
+     /**
+    *@brief  acessa a variável numero do endereço.
+    *@return unsigned int Retorna o numero.
+    */
     unsigned int get_numero();
-    //@brief acessa a variável complemento do endereço.
+    /**
+    *@brief  acessa a variável complemento do endereço.
+    *@return unsigned int Retorna o complemento.
+    */
     unsigned int get_complemento();
     /**
     *@brief converte um estado(int) em string
