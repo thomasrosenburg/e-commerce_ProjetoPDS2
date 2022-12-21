861f6e796542f4afea90459a0dccca913505b926
joseane13 <117231660+joseane13@users.noreply.github.com>
2022-12-21 12:01:12 -0300

    Update test_funcionario.cpp

--- test_funcionario.cpp
+++ test_funcionario.cpp
@@ -1,7 +1,8 @@
 #include "../include/funcionario.h"
 #include "../third_party/doctest.h"
 
-TEST_CASE("Testa construtor funcionario"){
+TEST_CASE("Testa construtor funcionario")
+{
     Funcionario funcionario = Funcionario("Thomas", "THOMPZERA", 13);
     CHECK_EQ(13, funcionario.get_registro());
     CHECK_EQ("Thomas", funcionario.get_nome());
