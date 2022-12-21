68435814f07f680b26cf8f849279deaecf88f943
joseane13 <117231660+joseane13@users.noreply.github.com>
2022-12-21 12:03:35 -0300

    Update test_produto.cpp

--- test_produto.cpp
+++ test_produto.cpp
@@ -1,9 +1,10 @@
-#include "../third_party/doctest.h"
 #include "../include/produto.h"
+#include "../third_party/doctest.h"
 
-TEST_CASE("Testa produto construtor"){
+TEST_CASE("Testa produto construtor")
+{
     Produto produto = Produto("Blusa nike", blusa, 1, 89.90, 1, "M", "Preto");
-    CHECK_EQ(0, produto.get_tipo());
+    CHECK_EQ(1, produto.get_tipo());
     CHECK_EQ("Blusa nike", produto.get_modelo());
     CHECK_EQ("M", produto.get_tamanho());
     CHECK_EQ(1, produto.get_codigo());
@@ -11,7 +12,8 @@ TEST_CASE("Testa produto construtor"){
     CHECK_EQ(1, produto._quantidade);
 }
 
-TEST_CASE("Testa função alterar quantidade"){
+TEST_CASE("Testa função alterar quantidade")
+{
     Produto produto = Produto("Blusa nike", blusa, 1, 89.90, 3, "M", "Preto");
     produto.alterar_quantidade(1);
     CHECK_EQ(4, produto._quantidade);
