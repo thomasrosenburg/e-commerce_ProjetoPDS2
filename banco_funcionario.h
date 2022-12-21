01e1a4c82dbb0982c4dafb62f42e8cc62e4fba84
alicelacerda <117699065+alicelacerda@users.noreply.github.com>
2022-12-20 21:04:19 -0300

    Update banco_funcionario.h

--- banco_funcionario.h
+++ banco_funcionario.h
@@ -7,31 +7,39 @@
 #include <string.h>
 #include <list>
 #include <iostream>
+/**
+ * @class Banco_Funcionarios
+ *
+ * @brief Esta classe é responsável por simular um banco de dados
+ * de funcionarios e  introduzir as funções relativas a essa classe.
+ */
 
 
 class Banco_Funcionario: public Banco_Dados
 {
     private:
     /**
-     *@brief lista de funcionários da loja
+     *@brief lista de funcionários da loja.
      */
     std::list<Funcionario> _banco_funcionarios;
     
     public:
     /**
-     *@brief adiciona um novo cadastro de funcionário
-     *@param nome Nome do novo funcionário
-     *@param login Login do novo funcionario
-     *@param registro Registro do novo funcionario
+     *@brief adiciona um novo cadastro de funcionário.
+     *@param nome Nome do novo funcionário.
+     *@param login Login do novo funcionario.
+     *@param registro Registro do novo funcionario.
      */
     void adiciona_funcionario(std:: string nome, std:: string login, unsigned int registro);
     /**
-     *@brief confere se o login está na lista
-     *@param login Login fornecido
+     *@brief confere se o login está na lista.
+     *@param login Login fornecido.
      */
     virtual bool confere_login(std::string login) override;
     /**
-     *@brief inicializa o banco de dados de funcionarios 
+     *@brief inicializa o banco de dados de funcionarios.
+     * Essa classe concretiza a funcao virtual herdada da
+     * superclasse Banco_Dados.
      */
     virtual void inicializa() override;
 
